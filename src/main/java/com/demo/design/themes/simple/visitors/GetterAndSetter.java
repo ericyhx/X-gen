76193e5d65829dfe41eae896be0286a278e236c7
ericyhx <282417075@qq.com>
2018-12-13 01:05:13 +0800

    继续完善

--- src/main/java/com/demo/design/themes/simple/visitors/GetterAndSetter.java
+++ src/main/java/com/demo/design/themes/simple/visitors/GetterAndSetter.java
@@ -22,8 +22,9 @@ public class GetterAndSetter implements Visitor {
         StringBuffer sb=new StringBuffer();
         for (int i = 0; i < voFields.length; i++) {
             sb.append("private "+voFieldsTypes[i]+" "+voFields[i]+";\n\t");
-//            voFields[i].r
-            sb.append("public "+voFieldsTypes[i]+" get"+voFields[i]+"() { return "+voFields[i]+"; }");
+            String temp = (voFields[i].substring(0, 1)).toUpperCase().concat(voFields[i].substring(1));
+            sb.append("public "+voFieldsTypes[i]+" get"+temp+"() { return "+voFields[i]+"; }\n\t");
+            sb.append("public void set"+temp+"(String "+voFields[i]+") { this."+voFields[i]+" = "+voFields[i]+"; }\n\t");
         }
         return sb.toString();
     }
